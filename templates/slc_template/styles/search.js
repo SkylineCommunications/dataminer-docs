4cd6f5b939e4a04b615d855fc7de656d76fa958e
Stanley Heyde <99649437+stanley-skyline@users.noreply.github.com>
2022-05-16 08:51:38 +0200

    Increase limit search results to 200
    Increasing the limit to 200, 25 was far too limited. Performance remains the same.

--- templates/slc_template/styles/search.js
+++ templates/slc_template/styles/search.js
@@ -81,7 +81,7 @@ $(function () {
   function createSearchQuery(searchTerm) {
     const apikey = '5630827C003AFD513AA4D8D21A0A79B7';
     const encodedQueryString = encodeURIComponent(searchTerm);
-    const limit = 25;
+    const limit = 200;
     return `https://docs-srch.search.windows.net/indexes/docs-blob-index/docs?api-version=2021-04-30-Preview&$top=${limit}&search=${encodedQueryString}&api-key=${apikey}`;
   }
 
