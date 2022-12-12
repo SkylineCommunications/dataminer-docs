e93bd7db16d9516a9b7797d34afc55708073805c
Pedro Debevere <pedro.debevere@skyline.be>
2022-12-12 11:42:41 +0100

    updated api

---
+++ src/Base Class Library/Common/Skyline/DataMiner/Library/Common/Rates/Enums/RateBase.cs
@@ -0,0 +1,20 @@
+﻿namespace Skyline.DataMiner.Library.Common.Rates
+{
+	/// <summary>
+	/// Determines whether the rate should be calculated per second, minute, hour or day.
+	/// </summary>
+	public enum RateBase
+	{
+		/// <summary>Rates will be calculated per second.</summary>
+		Second,
+
+		/// <summary>Rates will be calculated per minute.</summary>
+		Minute,
+
+		/// <summary>Rates will be calculated per hour.</summary>
+		Hour,
+
+		/// <summary>Rates will be calculated per day.</summary>
+		Day,
+	}
+}
