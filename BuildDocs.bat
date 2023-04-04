c6ce727093c0e4e688f555849088f06700ddd8ad
João Severino <joao.severino@skyline.be>
2023-04-04 15:23:36 +0100

    Automate build commands
    Run the commands as described in https://docs.dataminer.services/CONTRIBUTING.html#making-a-test-build

---
+++ BuildDocs.bat
@@ -0,0 +1,12 @@
+@echo off
+
+dotnet restore "src/NuGetPackages"
+dotnet build "src/NuGetPackages" --configuration Release
+docfx metadata
+docfx build
+
+if "%1"=="" (
+    docfx serve _site
+) else (
+    docfx serve _site -p %1
+)
