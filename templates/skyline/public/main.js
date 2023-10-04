a4a0eae8f1c6e89dcb01a7644a5a520943b819c8
Pedro Debevere <pedro.debevere@skyline.be>
2023-10-04 08:45:11 +0200

    modern template

---
+++ templates/skyline/public/main.js
@@ -0,0 +1,14 @@
+export default {
+	iconLinks: [
+    {
+      icon: 'github',
+      href: 'https://github.com/SkylineCommunications/dataminer-docs',
+      title: 'GitHub'
+    },
+    {
+      icon: 'twitter',
+      href: 'https://twitter.com/skydataminer',
+      title: 'Twitter'
+    }
+  ]
+}
