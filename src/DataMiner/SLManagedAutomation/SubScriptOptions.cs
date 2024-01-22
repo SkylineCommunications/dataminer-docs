f455cec0e70d7b156f61c6e56ffd90911ba26987
Jens Vandewalle <102030104+JensVandewalle@users.noreply.github.com>
2024-01-22 15:49:55 +0100

    Update src/DataMiner/SLManagedAutomation/SubScriptOptions.cs
    Co-authored-by: Pedro Debevere <97611683+PedroDebevere@users.noreply.github.com>

--- src/DataMiner/SLManagedAutomation/SubScriptOptions.cs
+++ src/DataMiner/SLManagedAutomation/SubScriptOptions.cs
@@ -68,6 +68,23 @@ namespace Skyline.DataMiner.Automation
 		/// Gets or sets a value indicating whether the script will return more detailed error information instead of throwing an exception. These can be retrieved with method <see cref="SubScriptOptions.GetErrorMessages"/>. This is supported from DataMiner 10.3.0/10.2.7 onwards.
 		/// </summary>
 		/// <value><c>true</c> if the script will return more detailed error info instead of throwing an exception; otherwise, <c>false</c>.</value>
+		/// <example>
+		/// <code>
+		/// var script = engine.PrepareSubScript("SRM_PFL_Ericsson_Decoder");
+		/// script.ExtendedErrorInfo = true;
+		/// script.SelectScriptParam("Info", "{}");
+		/// script.SelectScriptParam("ProfileInstance", "{}");
+		/// script.SelectDummy("dummy1", dmaId, elementId);
+		/// 
+		/// script.StartScript();
+		///
+		/// if (script.HadError)
+		/// {
+		///   string[] errors = script.GetErrorMessages();
+		///   // Handle errors.
+		///   }
+		/// </code>
+		/// </example>
 		public bool ExtendedErrorInfo { get; set; }
 
 		/// <summary>
