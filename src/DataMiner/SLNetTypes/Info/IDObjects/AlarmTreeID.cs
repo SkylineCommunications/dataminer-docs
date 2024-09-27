72e5a4c1cef21c45fae430c5b5e2098eb152b46a
Marieke Goethals <94605575+MariekeGO@users.noreply.github.com>
2024-09-27 10:52:51 +0200

    Update AlarmTreeID.cs

--- src/DataMiner/SLNetTypes/Info/IDObjects/AlarmTreeID.cs
+++ src/DataMiner/SLNetTypes/Info/IDObjects/AlarmTreeID.cs
@@ -10,7 +10,7 @@ namespace Skyline.DataMiner.Net
     [Serializable]
     public sealed class AlarmTreeID //: DMAObjectRef, IEquatable<AlarmTreeID>
     {
-        // default ctor is required for protobuf
+        // default constructor is required for protobuf
         public AlarmTreeID() { }
 
         /// <summary>
