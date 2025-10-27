99941743bf01cbcde6c2388173ab4dac914b5f70
Marieke Goethals <marieke.goethals@skyline.be>
2025-10-27 13:24:32 +0100

    RN 43964

--- src/DataMiner/SLManagedAutomation/IEngine.cs
+++ src/DataMiner/SLManagedAutomation/IEngine.cs
@@ -1479,7 +1479,7 @@ namespace Skyline.DataMiner.Automation
 		/// <value>The version of the web interactive Automation script UI.</value>
 		/// <remarks>
 		/// <para>Feature introduced in DataMiner 10.4.0 [CU21]/10.5.0 [CU9]/10.5.12 (RN 43875/43964).</para>
-		/// <para>The WebUIVersion is set to 'Default' by default. At present, this means that the old UI (V1) is used. You can instead set this to 'V2' to show the new UI or to 'V1' to make sure the old UI will continue to be used even if the default behavior changes. However, if the 'useNewIASInputComponents' URL parameter is used, this will override this configuration in the script.</para>
+		/// <para>The WebUIVersion is set to 'Default' by default. At present, this means that the old UI (V1) is used. You can instead set this to 'V2' to show the new UI or to 'V1' to make sure the old UI will continue to be used even if the default behavior changes. However, if the 'useNewIASInputComponents' URL parameter is used, this will override this configuration in the script (see <see href="xref:Configuring_app_actions_and_behavior_via_URL">Configuring app actions and behavior via the URL</see>).</para>
 		/// </remarks>
 		/// <example>
 		/// <code>
